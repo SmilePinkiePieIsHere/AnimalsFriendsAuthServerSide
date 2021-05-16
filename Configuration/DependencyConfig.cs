a1ccadaff93afd172f488defd0c70ee211737e62
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-05-16 17:44:27 +0300

    added posts in Configuration

--- Configuration/DependencyConfig.cs
+++ Configuration/DependencyConfig.cs
@@ -5,6 +5,7 @@ using AnimalsFriends.Controllers;
 using AnimalsFriends.Repositories;
 using AnimalsFriends.Services;
 using Microsoft.Extensions.DependencyInjection;
+using PostsFriends.Repositories;
 
 namespace AnimalsFriends.Configuration
 {
@@ -21,12 +22,14 @@ namespace AnimalsFriends.Configuration
         {
             services.AddTransient(typeof(UserController));
             services.AddTransient(typeof(AnimalsController));
+            services.AddTransient(typeof(PostsController));
         }
 
         private static void RegisterServices(IServiceCollection services)
         {
             services.AddTransient(typeof(IUserService), typeof(UserService));
             services.AddTransient(typeof(IAnimalService), typeof(AnimalService));
+            services.AddTransient(typeof(IPostService), typeof(PostService));
             services.AddTransient(typeof(IResourceOwnerPasswordValidator), typeof(ResourceOwnerPasswordValidator));
         }
 
@@ -34,6 +37,7 @@ namespace AnimalsFriends.Configuration
         {
             services.AddTransient(typeof(IUserRepository), typeof(UserRepository));
             services.AddTransient(typeof(IAnimalRepository), typeof(AnimalRepository));
+            services.AddTransient(typeof(IPostRepository), typeof(PostRepository));
         }
     }
 }
