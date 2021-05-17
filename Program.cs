fb3fb4ef4fc78cd95380d04769c1ec27b34b3276
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-05-17 21:20:19 +0300

    small edits

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using AnimalsFriends.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace AnimalsFriends
 {
@@ -8,6 +13,23 @@ namespace AnimalsFriends
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            //var host = CreateHostBuilder(args).Build();
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    try
+            //    {
+            //        var context = scope.ServiceProvider.GetService<AnimalsFriendsContext>();
+            //        context.Database.EnsureDeleted();
+            //        context.Database.Migrate();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred while migrating the database.");
+            //    }
+            //}
+            //host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
